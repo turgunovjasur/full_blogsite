97813c71fd78142b167cac149fa64e5b9fe7e8c2
Jasur <tjasur224@gmail.com>
2023-12-28 13:50:35 +0500

    views.py

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,8 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+
+from blogapp.forms import EmailPostForm, CommentForm
 from blogapp.models import Post
 
 
@@ -16,7 +19,27 @@ def post_detail(request, year, month, day, slug):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+
+    else:
+        comment_form =CommentForm()
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                   })
 
 
 def post_share(request, post_id):
@@ -24,8 +47,23 @@ def post_share(request, post_id):
     sent = False
 
     if request.method == 'POST':
-        form = EmailPostForm
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            print(cd)
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            title = f"{cd['name']} sizga {post.title} ni o'qishni taklif etadi."
+            message = f"{post.title} maqolasini o'qing: {post_url}\n\n" \
+                      f"{cd['name']}ning izohi: {cd['comments']}"
+            send_mail(title, message, 'matemmix@gmail.com', [cd['to']],
+                      fail_silently=False)
+            sent = True
 
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
 
 
 
