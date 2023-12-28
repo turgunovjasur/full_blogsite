30e94ac065353ca643be00b926092514e960943e
Jasur <tjasur224@gmail.com>
2023-12-28 13:36:14 +0500

    admin.py

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
          views.post_detail,
          name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -18,3 +18,24 @@ def post_detail(request, year, month, day, slug):
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
 
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
