5053a9b05bc383d046fe33c82603b474c8d79f54
Jasur <tjasur224@gmail.com>
2023-12-28 12:58:54 +0500

    html, css

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
@@ -17,7 +18,8 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique_for_date='publish')
-    author = models.ForeignKey()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
